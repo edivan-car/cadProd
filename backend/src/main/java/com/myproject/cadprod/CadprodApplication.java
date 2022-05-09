bf169801d292c8ae162725995dc113fca7e93d3e
edivan-car <edivam.c@gmail.com>
2022-05-09 16:09:09 -0300

    Create Project

---
+++ backend/src/main/java/com/myproject/cadprod/CadprodApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.cadprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadprodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadprodApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/myproject/cadprod/CadprodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.cadprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadprodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
